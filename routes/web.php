797fa8491690fe95feaecdf425fba11a7db6651f
Egberto Torres <etorres@app-peoplemarketing.com>
2022-04-06 20:38:24 -0500

    se agrega el proyecto

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
+
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [PageController::class, 'index'])->name('home');
+Route::get('curso/{course:slug}', [PageController::class, 'course'])->name('course');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
